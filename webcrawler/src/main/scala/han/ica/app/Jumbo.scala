6f7db9762b2af5949a16ad6fefec7fd82555520b
Bas van de Louw <basvdlouw@gmail.com>
2018-10-19 14:12:47 +0200

    added jumbo implementation

--- webcrawler/src/main/scala/han/ica/app/Jumbo.scala
+++ webcrawler/src/main/scala/han/ica/app/Jumbo.scala
@@ -1,15 +1,50 @@
 package han.ica.app
 
+import org.json4s.JsonAST.JDouble
+import org.json4s.native.JsonMethods
 import org.jsoup.Connection.Response
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 object Jumbo {
 
-  def extractProductInformation(response: Response): Unit = {
+  def extractProductInformation(response: Response): List[String] = {
     var productInfo = response.parse().getElementsByTag("div").asScala
-      .map{e => e.attr("data-jum-product-impression")}.toList
+      .map { e => e.attr("data-jum-product-impression") }
+    var productPrice = response.parse().getElementsByTag("input").asScala
+      .map { e => e.attr("jum-data-price") }
+    productInfo ++= productPrice
+    val list = filterArray(productInfo)
+    list.toList
+  }
 
+  def filterArray(array: mutable.Buffer[String]): mutable.Buffer[String] = {
+    var newArray = array.filter(_.length > 2)
+    convertToJson(newArray)
+    newArray
+  }
+
+  def convertToJson(array: mutable.Buffer[String]) : Unit = {
+    for (item <- array) {
+      if (isANumber(item)) {
+        val double: Double = item.toDouble
+        var json = JsonMethods.parse("""{ "price": +""" + double + "}")
+        array(array.indexOf(item)) = json.toString
+        JDouble.apply(item.toDouble)
+      }
+    }
+   array.grouped(array.length/2).toList
+  }
+
+  def isANumber(x: String): Boolean = x.matches("^-?[0-9]+(\\.[0-9]+)?$")
+
+}
+
+
+
+
+///-> e.attr("jum-price-format")}.toList)
 //
 //    for(product <- productInfo) {
 //      var json : JValue= product.insta
@@ -17,9 +52,4 @@ object Jumbo {
 //      println(product)
 //      println(product.extract)
 //    }
-  }
-}
 
-
-
-  ///-> e.attr("jum-price-format")}.toList)
--- webcrawler/src/main/scala/han/ica/app/Main.scala
+++ webcrawler/src/main/scala/han/ica/app/Main.scala
@@ -1,5 +1,7 @@
 package han.ica.app
 
+import org.json4s._
+import org.json4s.native.JsonMethods._
 import org.jsoup.Connection.Response
 import org.jsoup.Jsoup
 
@@ -10,17 +12,36 @@ import scala.collection.mutable.ListBuffer
 object Main {
 
   def main(args: Array[String]): Unit = {
+
+
     val url = "https://www.coop.nl/INTERSHOP/web/WFS/COOP-COOPBase-Site/nl_NL/-/EUR/ViewStandardCatalog-Browse?CatalogCategoryID=&PageSize=24&SortingAttribute=&CategoryName=21&CatalogID=COOP&SearchParameter=%26%40QueryTerm%3D*%26ContextCategoryUUID%3D548KAQCN5XAAAAFWkvJMRzCy%26OnlineFlag%3D1%26SupplierName%3DHertog%2BJan%26%40Sort.SalesRanking%3D0"
     val url2 = "https://www.jumbo.com/zoeken?SearchTerm=Hertog+jan&search="
-   // println(get(url).parse().html())
+    val url3 = "https://www.coop.nl/zoeken?SearchTerm=hertog%2520jan"
+    // println(get(url).parse().html())
     //get(url2)
+    // Jumbo.extractProductInformation(get(url2))
+    compareProductInformation(Coop.extractProductInformation(get(url3)), Jumbo.extractProductInformation(get(url2)))
+    //    crawler(url)
+    //    print(checkedUrls)
+    //    print(nonCheckedUrls)
 
-   println(Coop.extractProductInformation(get(url)))
-   // println(Jumbo.extractProductInformation(get(url2)))
-//    crawler(url)
-//    print(checkedUrls)
-//    print(nonCheckedUrls)
+  }
 
+
+  //Experimenting with functions as parameters
+  def compareProductInformation(coop: List[String], jumbo: List[String]): Unit = {
+//    println("Bij de COOP zijn bij de ingevoerde URL de volgende producten gevonden:")
+//    for (item <- coop) {
+//      val json = parse(item)
+//      println("Product naam is " + compact(render(json \ "name")) + " met de bijbehorende prijs " + compact(render(json \ "price")))
+//    }
+
+    println("Bij de Jumbo zijn bij de ingevoerde URL de volgende producten gevonden:")
+    for (i <- 0 to jumbo.length / 2) {
+      val json = parse(jumbo(i))
+      val json2 = jumbo(jumbo.length / 2 + i)
+      println("Product naam is " + compact(render(json \ "name")) + " met de bijbehorende prijs " + json2)
+    }
   }
 
 
@@ -29,7 +50,7 @@ object Main {
 
 
   //this function should later be refactored
-  //
+  //currently not used but should be used for the end product: get product information with only the index url e.g: https://www.ah.nl
   def crawler(url: String): Unit = {
     var currentUrl: String = url
     if (!checkedUrls.contains(currentUrl)) {
@@ -59,7 +80,6 @@ object Main {
       while (!b) {
         if (!checkedUrls.contains(nonCheckedUrls(i)) && isValidUrl(nonCheckedUrls(i))) {
           currentUrl = nonCheckedUrls(i)
-          // using temp boolean because I can't find the break command
           b = true
         }
         i += 1
@@ -83,20 +103,14 @@ object Main {
 
   def get(url: String): Response = {
     var response = Jsoup.connect(url).ignoreContentType(true).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1").execute()
-    if(response.statusCode() != 200) {
+    if (response.statusCode() != 200) {
       println("received error code :" + response.statusCode())
     }
     response
   }
 
-  def extractInformation(response: Response): Unit = {
-    //println(response.body())
-    //println(response.parse().body())
-  }
-
   def getAllUrlsFromLink(response: Response): List[String] = {
     if (response.contentType().startsWith("text/html") && response.statusCode() == 200) {
-      Jumbo.extractProductInformation(response)
       return response.parse().getElementsByTag("a").asScala.map(e => e.attr("href")).toList
     }
     null
